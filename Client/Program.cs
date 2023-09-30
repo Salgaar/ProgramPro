c513dd88190cf5a2b4d41cae2d7ca3a68f5cdebf
Christian Salgård Svinding <142394086+cssv56187@users.noreply.github.com>
2023-09-30 10:34:59 +0200

    auch

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,25 +4,32 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProgramPro.Client;
 using Radzen;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("ProgramPro.ServerAPI", client =>
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddHttpClient("ProgramPro.ServerAPI", client =>
+            {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                client.DefaultRequestHeaders.Add("ApiKey", "ProgramPro_#DONT#TOUCH#THIS.IS^^VERY**-12394827523235123.23.423,2134#RESTRICTED_API.Key");
+            });
+            //.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProgramPro.ServerAPI"));
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+
+            builder.Services.AddApiAuthorization();
+
+            await builder.Build().RunAsync();
+
+
+namespace ProgramPro.Client
 {
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-    client.DefaultRequestHeaders.Add("ApiKey", "ProgramPro_#DONT#TOUCH#THIS.IS^^VERY**-12394827523235123.23.423,2134#RESTRICTED_API.Key");
-});
-//.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProgramPro.ServerAPI"));
-
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>();
-
-builder.Services.AddApiAuthorization();
-
-await builder.Build().RunAsync();
+    public partial class Program { }
+}
---
+++ ProgramPro.Tests/ApiTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using ProgramPro.Shared.Models;
+using System.Net.Http.Json;
+using System.Net.Http;
+
+namespace IntegrationTests
+{
+    public class ApiTests
+    {
+        private readonly HttpClient _client;
+
+        public ApiTests()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:7134");
+            _client.DefaultRequestHeaders.Add("ApiKey", "ProgramPro_#DONT#TOUCH#THIS.IS^^VERY**-12394827523235123.23.423,2134#RESTRICTED_API.Key");
+        }
+
+        [Fact]
+        public async Task CanCreateNewTrainingProgram()
+        {
+            // Act
+            var response = await _client.PostAsJsonAsync("api/Trainingprograms", new TrainingProgram { Name = "New Program", DaysInASplit = 7, SplitNumber = 1, StartDate = DateTime.Now });
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Ensure the HTTP request was successful (status code 200-299).
+                                                // Add more assertions here to validate the response content or other aspects of the test.
+        }
+
+        [Fact]
+        public async Task CanGetTrainingPrograms()
+        {
+            // Act
+            var response = await _client.GetAsync("api/Trainingprograms");
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode); // Ensure the HTTP request was successful (status code 200-299).
+                                                // Add more assertions here to validate the response content or other aspects of the test.
+        }
+    }
+}
